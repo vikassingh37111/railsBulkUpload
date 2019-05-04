25f1d02741696a1a3463b55eab2b229a020c2abe
Vikas Singh <vikassingh37111@gmail.com>
2019-05-05 03:43:45 +0530

    IGNORE: Refactor commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # before_action :require_login
+
+  # private
+
+  # def require_login
+  #   unless user_signed_in?
+  #     flash[:error] = "You must be logged in to access this section"
+  #     # redirect_to new_user_session_path # halts request cycle
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get 'home/show'
   get 'home/edit'
 
+  # Rails.application.routes.draw do
+  #   devise_for :users, controllers: { sessions: 'users/sessions' }
+  # end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
