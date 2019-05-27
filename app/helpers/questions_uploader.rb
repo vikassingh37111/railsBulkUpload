e0f70ba75f15df835442188a242bba3698a69a72
Vikas Singh <vikassingh37111@gmail.com>
2019-05-28 03:45:17 +0530

    Bulk upload implemented

--- app/helpers/questions_uploader.rb
+++ app/helpers/questions_uploader.rb
@@ -1,23 +1,22 @@
-def QuestionsUploader
-	extend ActiveSupport::Concern
+module QuestionsUploader
 
-	def getLineData line
+	def self.get_line_data line
 		if line.match(/^\s*\d+\s*\./)
 			return { 
 				:type => 'question', 
 				:number => line.match(/^\s*\d+\s*/)[0].strip, 
 				:text => line.sub(/^\s*\d+\s*\./,'')
 			}
-		elsif line.match(/^\s*\(\s*\)\s*/)
+		elsif line.match(/^\s*\(.*\)\s*/)
 			return {
 				:type => 'option', 
-				:number => line.match(/^\s*\d+\s*/)[0].gsub(/\(|\)/ , '').strip, 
-				:text => line.sub(/^\s*\(\s*\)\s*/,'')
+				:number => line.match(/^\s*\(.*\)\s*/)[0].gsub(/\(|\)/ , '').strip, 
+				:text => line.sub(/^\s*\(.*\)\s*/,'')
 			}
 		elsif line.match(/^\s*[Ss]olution\s*:/)
 			return {
 				:type => 'solution', 
-				:solutions => line.sub(/^\s*[Ss]olution\s*:/).gsub(/\(|\)/ , '').split(',')
+				:solutions => line.sub(/^\s*[Ss]olution\s*:/, '').gsub(/\(|\)/ , '').split(',').map(&:strip)
 			}
 		else
 			return {
@@ -27,27 +26,29 @@ def QuestionsUploader
 		end
 	end
 
-	def check_duplicate_option_number current_question, option_number
+	def self.check_duplicate_option_number current_question, option_number
 		option_number.in? current_question[:options].map{|option| option[:option_number]}
 	end
 
-	def upload_qna_file file
+	def self.upload_qna_file filename
 		qna = []
 
+		error_occured = false
 		lineNumber = 0
-		File.open('/public/qna_file', 'r') do |f|
+		File.open("public/#{filename}" , 'r') do |f|
 		  f.each_line do |line|
 		  	line.strip!
 		  	next if line.empty?
-		    lineData = getLineData line
+		    lineData = self.get_line_data line
 		    if lineData[:type] == 'error'
-		   		puts "Upload Failed: \nLine #{lineNumber}: #{line} \n Error: #{lineData[:message]}"   	
+		   		puts "Upload Failed: \nLine #{lineNumber}: #{line} \n Error: #{lineData[:message]}"  
+		   		error_occured = true 	
 		    	break
 		    
 		    elsif lineData[:type] == 'question'
 		    	question_number = lineData[:number]
 		    	question_text = lineData[:text]
-		    	qna.push {:question_number => question_number, :question_text => question_text, :options => []}
+		    	qna.push({:question_number => question_number, :question_text => question_text, :options => []})
 		    
 		    elsif lineData[:type] == 'option'
 					current_question = qna.last
@@ -56,12 +57,14 @@ def QuestionsUploader
 		    	option_sequence = current_question[:options].length > 0 ? current_question[:options].last[:option_sequence] + 1 : 1
 		    	if option_number.blank?
 		    		puts "Upload Failed: \nLine #{lineNumber}: #{line} \n Error: Blank option number"
+		    		error_occured = true 	
 		    		break
-		    	if check_duplicate_option_number current_question, option_number
+		    	elsif self.check_duplicate_option_number current_question, option_number
 		    		puts "Upload Failed: \nLine #{lineNumber}: #{line} \n Error: Duplicate option found (#{option_number})"
+		    		error_occured = true 	
 		    		break
 		    	else
-		    		current_question[:options].push {:option_number => option_number, :option_text => option_text, :option_sequence => option_sequence}
+		    		current_question[:options].push({:option_number => option_number, :option_text => option_text, :option_sequence => option_sequence})
 		    	end
 
 		    elsif lineData[:type] == 'solution'
@@ -69,11 +72,14 @@ def QuestionsUploader
 		    	solutions = lineData[:solutions]
 		    	correct_options_found = false
 		    	current_question[:options].each do |option|
-		    		option[:correct] = true if option[:option_number].in? solutions
-		    		correct_options_found = true
+		    		if option[:option_number].in? solutions
+		    			option[:correct] = true
+		    			correct_options_found = true
+		    		end
 		    	end
 		    	unless correct_options_found
 		    		puts "Upload Failed: \nLine #{lineNumber}: #{line} \n Error: Solution not found in any of the options"
+		    		error_occured = true 	
 		    		break
 		    	end
 		    end
@@ -82,34 +88,45 @@ def QuestionsUploader
 		end
 
 		# Passing a hash now 
-		upload_qna_json_to_db(qna)
+		# This can be used to directlt pass a json
+		self.upload_qna_json_to_db(qna) unless error_occured
+	end
+
+	def self.skip_already_uploaded data		
+		question_numbers = data.map{|question| question[:question_number]}
+		already_uploaded_question_numbers = Question.where(:question_number => question_numbers).pluck(:question_number)
+		data = data.reject{|question| question[:question_number].to_i.in? already_uploaded_question_numbers}
 	end
 
 	# Bulk Insert
-	def upload_qna_json_to_db data
+	def self.upload_qna_json_to_db data
+		# User 0 represents backend job
+		current_user = 0 unless current_user.present?
+		data = self.skip_already_uploaded data
+
 		ActiveRecord::Base.transaction do
   		questions = []
 			data.each do |question|
-			  question = Question.new(
+			  question_obj = Question.new(
 			  	question_number: question[:question_number], 
-			  	text: question[:question_text], 
-			  	created_by: current_user, 
+			  	text: question[:question_text],
+			  	created_by: current_user,
 			  	updated_by: current_user
 			  )
 				question[:options].each do |option|
-					question.options.build(
+					question_obj.options.build(
 						option_number: option[:option_number], 
 						text: option[:option_text],
 						sequence: option[:option_sequence],
 						correct: option[:correct],
-						created_by: current_user,
-						updated_by: current_user
+			  		created_by: current_user,
+			  		updated_by: current_user
 					)
 				end
-				questions << question
+				questions << question_obj
 			end
 			Question.import questions, recursive: true
 		end
 	end 
-	
+
 end
